2dc710db9fadaf258a7a4aaa8ebe48c0a5c0b529
unknown <eko.andri@icloud.com>
2018-05-31 01:22:10 +0100

    refactor Users.js

--- client-react/src/components/Data/Users.js
+++ client-react/src/components/Data/Users.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import axios from 'axios';
 
-export default class Users extends React.Component {
+export default class UserList extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      Users: []
+      users: []
     }
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/Users').then(res => {
-      // axios.get('https://app.subarnanto.com/api/Users').then(res => {
-      this.setState({ Users: res.data });
-      console.log({ Users: res.data });
+    axios.get('http://localhost:5000/users').then(res => {
+      // axios.get('https://app.subarnanto.com/api/users').then(res => {
+      this.setState({ users: res.data });
+      console.log({ users: res.data });
     });
   }
 
   render() {
-    return this.state.Users.map(itemList => {
-      let item = itemList;
+    return this.state.users.map(user => {
       return (
         <div className="card margin-card">
           <div className="card-content">
             <div>
-              <h4>Nama:  {item.fullname} </h4>
-              <h4>ID Project:  {item.userid} </h4>
-              <h4>Group:  {item.usergroup} </h4>
-              <h4>Email:  {item.emailid} </h4>
-              <h4>Title:  {item.title} </h4>
+              <h4>Nama:       {user.fullname} </h4>
+              <h4>ID Project: {user.userid} </h4>
+              <h4>Group:      {user.usergroup} </h4>
+              <h4>Email:      {user.emailid} </h4>
+              <h4>Title:      {user.title} </h4>
             </div>
           </div>
         </div>
